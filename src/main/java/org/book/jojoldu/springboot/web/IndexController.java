5d78144a151fb29d2fb6f87f3bd2a38738aab28b
Jeongkyo Kim <chavitak@gmail.com>
2022-04-21 22:54:34 +0900

    feat: google OAuth를 추가한다

--- src/main/java/org/book/jojoldu/springboot/web/IndexController.java
+++ src/main/java/org/book/jojoldu/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package org.book.jojoldu.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.book.jojoldu.springboot.config.auth.LoginUser;
+import org.book.jojoldu.springboot.config.auth.dto.SessionUser;
 import org.book.jojoldu.springboot.service.posts.PostsService;
 import org.book.jojoldu.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
